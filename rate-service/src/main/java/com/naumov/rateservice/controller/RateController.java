b7aa2598cf5caeab6bb81a6096ffeab2fc836841
naumovHellsCoder <naumov0coder@gmail.com>
2022-12-14 22:45:05 +0300

    transaction-service start dev

--- rate-service/src/main/java/com/naumov/rateservice/controller/RateController.java
+++ rate-service/src/main/java/com/naumov/rateservice/controller/RateController.java
@@ -1,14 +1,23 @@
 package com.naumov.rateservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/rate/")
 public class RateController {
+    private final Logger logger = LoggerFactory.getLogger(RateController.class);
     @GetMapping("/")
-    public Object getMain(){
-        return new Object();
+    public Map<String, String> getMain(@RequestHeader Map<String, String> headers){
+        System.out.println(headers);
+        String data = headers.get("authorization");
+        logger.info(data);
+        return headers;
     }
 }
